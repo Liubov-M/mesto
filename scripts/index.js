bbb2611b03be37e0abf656804b2f98ff544091c3
Liubov Mikhnovets <zaiceva_1983@mail.ru>
2023-03-13 14:10:24 +0300

    Вторая итерация ПР-4

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
-const popupElement = document.querySelector('.popup');
+const popupElement = document.querySelector('[name="profile-form"]');
+const popupOverlay = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-let nameInput = popupElement.querySelector('.popup__text_name');
-let jobInput = popupElement.querySelector('.popup__text_job');
+let nameInput = popupElement.querySelector('[name="firstname"]');
+let jobInput = popupElement.querySelector('[name="job"]');
 const popupSubmitButton = popupElement.querySelector('.popup__submit-button');
 let firstName = document.querySelector('.profile__info-name');
 let aboutMe = document.querySelector('.profile__info-job');
 
-function handleFormSubmit (evt) {
-  evt.preventDefault();
-  firstName.textContent = nameInput.value;
-  aboutMe.textContent = jobInput.value;
-}
-
 const openPopup = function () {
-  popupElement.classList.add('popup_opened');
+  popupOverlay.classList.add('popup_opened');
   nameInput.value = firstName.textContent;
   jobInput.value = aboutMe.textContent;
 };
 
 const closePopup = function () {
-  popupElement.classList.remove('popup_opened');
+  popupOverlay.classList.remove('popup_opened');
 }
 
-
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  firstName.textContent = nameInput.value;
+  aboutMe.textContent = jobInput.value;
+  closePopup();
+}
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('submit', handleFormSubmit);
-popupSubmitButton.addEventListener('click', closePopup);
